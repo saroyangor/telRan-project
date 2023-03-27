094e7c12e1c1e23b5317d5d41d7c8dcc5eacb3b9
saroyangor <dr.saroyan98@gmail.com>
2023-03-27 23:52:43 +0400

    add routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { MainPage } from "@/pages/MainPage";
-import { AboutPage } from "@/pages/AboutPage";
 import { classNames } from "@/shared/lib/classNames";
 
 import { useTheme } from "./providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
 
 import './styles/index.scss';
 
@@ -18,12 +17,7 @@ const App = () => {
       <Link to='/'>Главная</Link>
       <Link to='/about'>О нас</Link>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
